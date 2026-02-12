d05879f8e5eb51884be4305a0863358c7785d2a7
Peter Theobald <peter@petertheobald.com>
2026-02-12 12:47:21 -0800

    rm disqus ads

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -1,1 +1,20 @@
-// custom code goes here
+document.addEventListener("DOMContentLoaded", function() {
+	var disqus = document.getElementById("disqus_thread");
+	if (!disqus || !window.MutationObserver) {
+		return;
+	}
+
+	var observer = new MutationObserver(function() {
+		var iframes = disqus.getElementsByTagName("iframe");
+		if (iframes.length > 1) {
+			var commentsIframe = iframes[1];
+			while (disqus.firstChild) {
+				disqus.removeChild(disqus.firstChild);
+			}
+			disqus.appendChild(commentsIframe);
+			observer.disconnect();
+		}
+	});
+
+	observer.observe(disqus, { childList: true, subtree: true });
+});
